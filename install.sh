fac37b514b19587d3701de36399ad60367b64012
Bartlomiej Wilczynski <bwilczyn@gmail.com>
2015-05-13 21:17:43 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+
+sh install/braw.sh
+sh install/ohmyzsh.sh
+
+ln -sf symlinks/zshrc ~/.zshrc
---
+++ install/brew.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+type brew >/dev/null 2>&1 || {
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+brew install autojump
+brew install git
+brew install python
+brew install rbenv
+brew install task
+brew install xtail
+
+brew install caskroom/cask/brew-cask
+
+brew cask install sublime-text
+brew cask install avast
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install flux
