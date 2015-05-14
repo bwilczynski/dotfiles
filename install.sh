f01fffec6c2c557c493656183a2144ff81994aad
Bartek Wilczynski <bwilczynski@apreel.com>
2015-05-14 20:31:30 +0200

    Fix installation script

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 DOTFILES_ROOT=$(pwd)
 SYMLINKS_ROOT=$DOTFILES_ROOT/symlinks
+SUBLIME_SETTINGS=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 sh install/braw.sh
 sh install/ohmyzsh.sh
 
-ln -sf $SYMLINKS_ROOT/zshrc ~/.zshrc
-ln -sf $DOTFILES_ROOT/sublime ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -vsf "${SYMLINKS_ROOT}/zshrc" ~/.zshrc
+rm -r "${SUBLIME_SETTINGS}"
+ln -vsf "${DOTFILES_ROOT}/sublime" "${SUBLIME_SETTINGS}"
