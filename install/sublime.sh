d199a8983822f2e26736467750eb2b9100f3e0b1
Bartek Wilczynski <bwilczynski@apreel.com>
2017-01-17 21:02:38 +0100

    Update sublime settings and plugins

--- install/sublime.sh
+++ install/sublime.sh
@@ -1,6 +1,6 @@
 SUBLIME_INSTALLED_PACKAGES=~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
-SUBLIME_PACKGE_CONTROL=$SUBLIME_INSTALLED_PACKAGES/Package\ Control.sublime-package
+SUBLIME_PACKAGE_CONTROL=$SUBLIME_INSTALLED_PACKAGES/Package\ Control.sublime-package
 
-if [ ! -f "$SUBLIME_PACKGE_CONTROL" ]; then
-	curl https://packagecontrol.io/Package%20Control.sublime-package > "$SUBLIME_PACKGE_CONTROL"
+if [ ! -f "$SUBLIME_PACKAGE_CONTROL" ]; then
+	curl https://packagecontrol.io/Package%20Control.sublime-package > "$SUBLIME_PACKAGE_CONTROL"
 fi
