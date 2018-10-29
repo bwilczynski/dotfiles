94fed48682da3521610fc64911bd415250a7b59a
Bartlomiej Wilczynski <bartlomiej.wilczynski@itmagination.com>
2018-10-29 16:29:57 +0100

    Set theme to spaceship

--- install/ohmyzsh.sh
+++ install/ohmyzsh.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 OH_MY_ZSH=~/.oh-my-zsh
+ZSH_CUSTOM="$OH_MY_ZSH/custom"
 
 if [ ! -d "$OH_MY_ZSH" ]; then
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 fi
 
-cp ohmyzsh/*.zsh "$OH_MY_ZSH/custom/"
+cp ohmyzsh/*.zsh $ZSH_CUSTOM
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" \
+  && ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
--- install/ohmyzsh/shortcuts.zsh
+++ install/ohmyzsh/shortcuts.zsh
@@ -1,1 +1,8 @@
-apreel=~/Projects/apreel
+lf() {
+    fzf --preview '[[ $(file --mime {}) =~ binary ]] &&
+                    echo {} is a binary file ||
+                    (highlight -O ansi -l {} ||
+                    coderay {} ||
+                    rougify {} ||
+                    cat {}) 2> /dev/null | head -500'
+}
