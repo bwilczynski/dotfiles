3bb79fa3c8721a91acaebb707981cef39b84d07f
Bartlomiej Wilczynski <me@bwilczynski.com>
2025-11-07 19:16:40 +0100

    Add hammerspoon config

---
+++ .hammerspoon/launcher.lua
@@ -0,0 +1,15 @@
+hs.loadSpoon("AppLauncher")
+
+spoon.AppLauncher.modifiers = {"ctrl", "alt", "cmd", "shift"}
+spoon.AppLauncher:bindHotkeys({
+  c = "Calendar",
+  d = "2Do",
+  f = "Finder",
+  k = "Firefox",
+  m = "Spotify",
+  n = "Notes",
+  p = "Bitwarden",
+  s = "Slack",
+  t = "iTerm",
+  v = "Visual Studio Code",
+})
