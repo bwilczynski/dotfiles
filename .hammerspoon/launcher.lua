62b244c2db82a55df7897b9305cd753f03660856
Bartlomiej Wilczynski <me@bwilczynski.com>
2025-11-08 18:05:04 +0100

    Add hammerspoon and tmux configuration

--- .hammerspoon/launcher.lua
+++ .hammerspoon/launcher.lua
@@ -1,15 +1,17 @@
 hs.loadSpoon("AppLauncher")
 
-spoon.AppLauncher.modifiers = {"ctrl", "alt", "cmd", "shift"}
+spoon.AppLauncher.modifiers = { "ctrl", "alt", "cmd", "shift" }
 spoon.AppLauncher:bindHotkeys({
-  c = "Calendar",
-  d = "2Do",
-  f = "Finder",
-  k = "Firefox",
-  m = "Spotify",
-  n = "Notes",
-  p = "Bitwarden",
-  s = "Slack",
-  t = "iTerm",
-  v = "Visual Studio Code",
+	c = "Calendar",
+	d = "2Do",
+	f = "Finder",
+	k = "Firefox",
+	m = "Spotify",
+	n = "Notes",
+	p = "Bitwarden",
+	s = "Slack",
+	t = "iTerm",
+	v = "Visual Studio Code",
+	y = "YouTube",
 })
+
