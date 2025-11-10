a3d20c018a97b94e6731189f0f06ca6bd501d74f
Bartlomiej Wilczynski <me@bwilczynski.com>
2025-11-10 16:27:17 +0100

    Use catpuccin-mocha for fzf

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,19 @@
 # Setup fzf
-# ---------
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
   export PATH="$PATH:/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
-# ---------------
 [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
-# ------------
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+# Catppuccin fzf theme
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+--color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+--color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
+--color=selected-bg:#45475A \
+--color=border:#6C7086,label:#CDD6F4"
 
