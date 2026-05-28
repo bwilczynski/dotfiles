1167bb57502c64d3abdecef71729761a6b3aee46
Bartlomiej Wilczynski <me@bwilczynski.com>
2026-05-28 18:24:57 +0200

    Add plantuml-syntax and preview helper

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.filetype.add({
+  extension = {
+    puml = "plantuml",
+    plantuml = "plantuml",
+    pu = "plantuml",
+  },
+})
+
+vim.api.nvim_create_user_command("PumlPreview", function()
+  local file = vim.fn.expand("%")
+  local png = vim.fn.expand("%:r") .. ".png"
+  vim.fn.system({ "plantuml", file })
+  vim.fn.system({ "open", png })
+end, {})
