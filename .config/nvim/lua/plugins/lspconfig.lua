86d2bf91a9371168ccc3bc2a4a174b910610cb79
Bartłomiej Wilczyński <me@bwilczynski.com>
2026-08-08 19:43:14 +0200

    feat: support for iOS development in neovim

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -15,6 +15,32 @@ return {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
         vacuum = {},
+        sourcekit = {
+          cmd = {
+            "sourcekit-lsp",
+          },
+
+          filetypes = {
+            "swift",
+            "objective-c",
+            "objective-cpp",
+          },
+
+          root_dir = function(bufnr, on_dir)
+            local fname = vim.api.nvim_buf_get_name(bufnr)
+
+            local root = vim.fs.root(bufnr, {
+              "Package.swift",
+              ".git",
+              "*.xcodeproj",
+              "*.xcworkspace",
+            })
+
+            if root then
+              on_dir(root)
+            end
+          end,
+        },
       },
     },
   },
